3b1fc0a1bf5dfd3ca33116fe2fecb4e791f74fe1
zhuyuqian <yuqian.zhu@thoughtowrks.com>
2019-04-20 16:59:35 +0800

    [zhuyuqian] 新增注册时username和password验证

--- src/main/java/com/thoughtworks/nho/controller/UserController.java
+++ src/main/java/com/thoughtworks/nho/controller/UserController.java
@@ -3,6 +3,7 @@ package com.thoughtworks.nho.controller;
 import com.alibaba.fastjson.JSONObject;
 import com.thoughtworks.nho.ResponseMessage;
 import com.thoughtworks.nho.model.RegisterRequestVO;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,8 +22,19 @@ public class UserController {
 
     @PostMapping("/register")
     public ResponseMessage register(@RequestBody RegisterRequestVO registerRequestVO){
+        if(StringUtils.isEmpty(registerRequestVO.getUsername()) || !verifyUsernameAndPassword(registerRequestVO.getUsername())){
+            return ResponseMessage.error(100, "账号长度应为6~16字母");
+        }
+        if(StringUtils.isEmpty(registerRequestVO.getUsername()) || !verifyUsernameAndPassword(registerRequestVO.getPassword())){
+            return ResponseMessage.error(101, "密码长度应为6~16字母");
+        }
+
         JSONObject data = new JSONObject();
         data.put("token", "this is a test token");
         return ResponseMessage.ok(data);
+    }
+
+    private boolean verifyUsernameAndPassword(String value){
+        return value.matches("[a-zA-Z]{6,16}");
     }
 }
